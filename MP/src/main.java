7671ee6c36b6fecb5bf2bb5bdd88a9178f26a460
TomerKobrinsky <tkobrinsky@gmail.com>
2018-01-03 22:42:15 +0200

    no message

--- MP/src/main.java
+++ MP/src/main.java
@@ -4,21 +4,17 @@ import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
 
 public class main {
-	public static void main(String[] args){
+    public static void main(String[] args) {
 
-		session a= new session();
+        session a = new session();
 
-		JFrame frame = new JFrame();
-		welcomeForm wf = new welcomeForm(a);
-		frame.setContentPane(wf.getPanel());
-		wf.setFrame(frame);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1500, 1000); // << not working!!!
-		frame.setVisible(true);
-<<<<<<< HEAD
-		*/
-=======
->>>>>>> 19c1bc037488a0a6787255eca0d0ec0abf4bfce3
+        JFrame frame = new JFrame();
+        welcomeForm wf = new welcomeForm(a);
+        frame.setContentPane(wf.getPanel());
+        wf.setFrame(frame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1500, 1000); // << not working!!!
+        frame.setVisible(true);
 
-	}
+    }
 }
